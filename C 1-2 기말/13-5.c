88bf44451af40071119a77b922cb737fc6933aae
bokyung <bokyung082808@gmail.com>
2023-12-11 17:51:39 +0900

    댜

--- C 1-2 기말/13-5.c
+++ C 1-2 기말/13-5.c
@@ -1,1 +1,15 @@
-#include <stdio.h>
+/*#include <stdio.h>
+int main() {
+	FILE* fw = fopen("output.txt", "w");
+	if (fw == NULL) {
+		printf("File open failed");
+		return 1;
+	}
+	fprintf(fw, "you can't see this text");
+	int temp;
+	printf("pause\n");
+	scanf("%d", &temp);
+	printf("done");
+	fclose(fw);//fclose 를 해줘야 텍스트가 작성된걸 확인할 수있음
+	return 0;
+}*/
